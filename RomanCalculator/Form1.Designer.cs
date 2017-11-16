adf2423da54ed3e0f001507870ff517821001766
Wafflebasket <Wafflebasket@outlook.com>
2017-11-16 11:58:11 +0100

    first

--- RomanCalculator/Form1.Designer.cs
+++ RomanCalculator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RomanCalculator
 {
-    partial class Form1
+    partial class RomanCalculator
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.romanRadioBtn = new System.Windows.Forms.RadioButton();
+            this.normalRadioBtn = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(52, 170);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(50, 50);
+            this.btn7.TabIndex = 0;
+            this.btn7.Text = "VII";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(108, 170);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(50, 50);
+            this.btn8.TabIndex = 1;
+            this.btn8.Text = "VIII";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(164, 170);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(50, 50);
+            this.btn9.TabIndex = 2;
+            this.btn9.Text = "IX";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(164, 226);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(50, 50);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "VI";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(108, 226);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(50, 50);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "V";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(52, 226);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(50, 50);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "IV";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(164, 282);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(50, 50);
+            this.btn3.TabIndex = 8;
+            this.btn3.Text = "III";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(108, 282);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(50, 50);
+            this.btn2.TabIndex = 7;
+            this.btn2.Text = "II";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(52, 282);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(50, 50);
+            this.btn1.TabIndex = 6;
+            this.btn1.Text = "I";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(52, 68);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(256, 96);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(159, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Computatrum";
+            // 
+            // romanRadioBtn
+            // 
+            this.romanRadioBtn.AutoSize = true;
+            this.romanRadioBtn.Location = new System.Drawing.Point(336, 68);
+            this.romanRadioBtn.Name = "romanRadioBtn";
+            this.romanRadioBtn.Size = new System.Drawing.Size(138, 17);
+            this.romanRadioBtn.TabIndex = 11;
+            this.romanRadioBtn.TabStop = true;
+            this.romanRadioBtn.Text = "Old Skool Roman Mode";
+            this.romanRadioBtn.UseVisualStyleBackColor = true;
+            this.romanRadioBtn.CheckedChanged += new System.EventHandler(this.romanRadioBtn_CheckedChanged);
+            // 
+            // normalRadioBtn
+            // 
+            this.normalRadioBtn.AutoSize = true;
+            this.normalRadioBtn.Location = new System.Drawing.Point(336, 147);
+            this.normalRadioBtn.Name = "normalRadioBtn";
+            this.normalRadioBtn.Size = new System.Drawing.Size(151, 17);
+            this.normalRadioBtn.TabIndex = 12;
+            this.normalRadioBtn.TabStop = true;
+            this.normalRadioBtn.Text = "Stop giving me headaches";
+            this.normalRadioBtn.UseVisualStyleBackColor = true;
+            this.normalRadioBtn.CheckedChanged += new System.EventHandler(this.normalRadioBtn_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(220, 282);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "/";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(220, 226);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(220, 170);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "+";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(52, 339);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 50);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "=";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(164, 339);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(106, 50);
+            this.btnClear.TabIndex = 17;
+            this.btnClear.Text = "Diluo";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // RomanCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.normalRadioBtn);
+            this.Controls.Add(this.romanRadioBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Name = "RomanCalculator";
+            this.Text = "Tu quoque Brute";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton romanRadioBtn;
+        private System.Windows.Forms.RadioButton normalRadioBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
--- RomanCalculator/Form1.cs
+++ RomanCalculator/Form1.cs
@@ -10,11 +10,58 @@ using System.Windows.Forms;
 
 namespace RomanCalculator
 {
-    public partial class Form1 : Form
+    public partial class RomanCalculator : Form
     {
-        public Form1()
+        public RomanCalculator()
         {
             InitializeComponent();
+            romanRadioBtn.Checked = true;
+        }
+
+        private void normalRadioBtn_OnClick(object sender, EventArgs e)
+        {
+            if (romanRadioBtn.Checked == true)
+            {
+                normalRadioBtn.Checked = true;
+                romanRadioBtn.Checked = false;
+            }
+        }
+
+        private void romanRadioBtn_OnClick(object sender, EventArgs e)
+        {
+            if (normalRadioBtn.Checked == true)
+            {
+                romanRadioBtn.Checked = true;
+                normalRadioBtn.Checked = false;
+            }
+        }
+        private void normalRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            btn1.Text = "1";
+            btn2.Text = "2";
+            btn3.Text = "3";
+            btn4.Text = "4";
+            btn5.Text = "5";
+            btn6.Text = "6";
+            btn7.Text = "7";
+            btn8.Text = "8";
+            btn9.Text = "9";
+            btnClear.Text = "C";
+        }
+        
+
+        private void romanRadioBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            btn1.Text = "I";
+            btn2.Text = "II";
+            btn3.Text = "III";
+            btn4.Text = "IV";
+            btn5.Text = "V";
+            btn6.Text = "VI";
+            btn7.Text = "VII";
+            btn8.Text = "VIII";
+            btn9.Text = "IX";
+            btnClear.Text = "Diluo";
         }
     }
 }
